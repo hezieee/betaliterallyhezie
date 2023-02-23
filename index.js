d585087391f5844c944cd12354d58c23efc52706
hezieee <82689551+hezieee@users.noreply.github.com>
2023-02-22 21:36:03 -0500

    version 2.1

--- index.js
+++ index.js
@@ -1,36 +1,57 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
+tabs.forEach(tab =>{
+  tab.addEventListener('click', () =>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tc =>{
+      tc.classList.remove('filter__active')
+    })
+    target.classList.add('filters__active')
+
+    tabs.forEach(t => {
+      t.classList.remove('filter-tab-active')
+    })
+    tab.classList.add('filter-tab-active')
   })
 })
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
 
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon ==='ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.profile__border')
+sr.reveal('.profile__name', {delay: 500})
+sr.reveal('.profile__profession', {delay: 600})
+sr.reveal('.profile__social', {delay: 700})
+sr.reveal('.profile__info-group', {interval:100, delay: 700})
+sr.reveal('.profile__buttons', {delay: 800})
+sr.reveal('.filters__content', {delay: 900})
+sr.reveal('.filters', {delay: 1000})
+sr.reveal('.footer__copy', { delay: 1400})
